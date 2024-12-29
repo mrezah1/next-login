fa843347976c7d6359d161bc39ffda63194972b4
Mhmdreza <Mrhhajiyan@gmail.com>
2024-12-29 16:27:45 +0330

    middleware handle protected route

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,12 @@ import { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   // Access headers
   const token = request.cookies.get("token");
-  if (!token) return NextResponse.redirect(new URL("/auth", request.url));
+  if (!token) {
+    const redirectUrl = new URL("/auth", request.url);
+    redirectUrl.searchParams.set("redirect", request.nextUrl.pathname); // Add the redirect parameter
+    return NextResponse.redirect(redirectUrl);
+  }
+  return NextResponse.next();
 }
 
 // Apply to all routes or specific routes
